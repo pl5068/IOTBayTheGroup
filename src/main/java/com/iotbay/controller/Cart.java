7c13adc01f9e11244147a81c962e931612c2dd79
John <johnreeve212@gmail.com>
2024-05-19 01:50:19 +1000

    paolo this is for you my sheet is geeking on god

--- src/main/java/com/iotbay/controller/Cart.java
+++ src/main/java/com/iotbay/controller/Cart.java
@@ -15,93 +15,49 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-
 public class Cart extends HttpServlet {
+
     @Override
-   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-       
-  
-            
-           // Retrieve form data
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        
+        
+        System.out.print("Hello");
+        // Retrieve form data
         int productId = Integer.parseInt(request.getParameter("productId"));
         double price = Double.parseDouble(request.getParameter("price"));
-        int unitStock = Integer.parseInt(request.getParameter("unitStock"));
-       
-           request.setCharacterEncoding("UTF-8");
-            String email = request.getParameter("email");
-            String password = request.getParameter("password");
-         
-           
-            String dob = request.getParameter("dob");
-            String phoneNumber = request.getParameter("phoneNumber");
-          
 
-              HttpSession session = request.getSession();
-              
-            User user = (User) session.getAttribute("user") ;
-            
-            int customerId = user.getId() ;
-            String firstName = user.getFirstName();
-                   
-            String lastName = user.getLastName(); 
-            
-      
-    
+        request.setCharacterEncoding("UTF-8");
 
-          
-    
+        HttpSession session = request.getSession();
+
+        User user = (User) session.getAttribute("user");
+
+        int customerId = user.getId();
+
         // Retrieve form data
-   
         String street = request.getParameter("street");
         String city = request.getParameter("city");
         String state = request.getParameter("state");
         String postcode = request.getParameter("postcode");
-        
-        
-         
-       
-         
-      
-     
 
-       
-       try {
-              DBConnector connector = new DBConnector();
+        try {
+            DBConnector connector = new DBConnector();
 
             Connection conn = connector.openConnection();
 
             DBManager db = new DBManager(conn);
-       
 
- 
-        
-       
-        
-     
-        int quantity = Integer.parseInt(request.getParameter("quantity"));
+            // Get customer id from session
+            db.makeOrder(customerId, productId, price, street, city, state, postcode);
 
-        // Get customer id from session
- 
-         
+            request.getRequestDispatcher("checkoutPage.jsp").forward(request, response);
+            conn.close();
 
-       
-
-        if(unitStock < 1 ){
-           System.out.print("No stock available");
-        } else {
-           db.retrieveItem(productId, quantity);
-           // Add item to the cart
-           db.makeOrder(customerId, productId, price , street, city, state, postcode);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Cart.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-     request.getRequestDispatcher("checkoutPage.jsp").forward(request, response);
-               conn.close();
-               
-    } catch (ClassNotFoundException | SQLException ex) {
-        Logger.getLogger(Cart.class.getName()).log(Level.SEVERE, null, ex);
     }
-       
-       
-}
 }
